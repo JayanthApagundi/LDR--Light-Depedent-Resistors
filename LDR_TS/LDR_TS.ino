0158a16e6011804b23139fc9977ff644f708a223
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2019-11-21 18:55:31 +0530

    Update LDR_TS.ino

--- LDR_TS/LDR_TS.ino
+++ LDR_TS/LDR_TS.ino
@@ -1,13 +1,13 @@
 #include <ESP8266WiFi.h>;
 #include <WiFiClient.h>;
 #include <ThingSpeak.h>;
-const char* ssid = "JioFi2_12C19E"; //Your Network SSID
-const char* password = "ihmi9en3bd"; //Your Network Password
+const char* ssid = "XXXXXXXXXXXXXXXXXXX"; //Your Network SSID
+const char* password = "XXXXXXXXXXXXXXXXXX"; //Your Network Password
 int val;
 int LDRpin = A0; //LDR Pin Connected at A0 Pin 
 WiFiClient client;
-unsigned long myChannelNumber = 883762; //Your Channel Number (Without Brackets)
-const char * myWriteAPIKey = "7EP9B29LW697YEGB"; //Your Write API Key
+unsigned long myChannelNumber = XXXXXXX; //Your Channel Number (Without Brackets)
+const char * myWriteAPIKey = "XXXXXXXXXXXXXXXXX"; //Your Write API Key
 void setup()
 {
 Serial.begin(9600);
